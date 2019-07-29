132679b88b593ec0d71e0f36aaa1965d0279eb3d
Slash.Huang <slash.huang@dfi.com>
2019-07-29 13:28:49 +0800

    [driver-plm451-linux_gpio_gpe_pch  gpio-test commit]
    1. user-land control NCT6106_GPO23, NCT6106_GPO26
    2. user-land get key status form kernel ACPI interrupt
    
    1. user-land control gpo by /dev/GPIO_DEVICE by GPIO_DEVICE.ko
    2. Customer can add work in handle
    
    for plm451 use
    
    Initial Commit

--- test_ap/gpio_test.c
+++ test_ap/gpio_test.c
@@ -12,73 +12,75 @@
 #include <stdbool.h>
 
 
-#define		GPIO_IOCTL_MAGIC		    'i'
-#define		GPIO_IOCTL_SET_OUTPUT		_IOW(GPIO_IOCTL_MAGIC, 1 ,int)
-#define		GPIO_IOCTL_GET_INPUT		_IOW(GPIO_IOCTL_MAGIC, 2 ,int)
-#define		GPIO_IOCTL_SET_PID_CMD		_IOW(GPIO_IOCTL_MAGIC, 3 ,int)
-#define 	DFI_GPIO_DEV			    "/dev/GPIO_DEVICE"
+#define GPIO_IOCTL_MAGIC 'i'
+#define GPIO_IOCTL_SET_OUTPUT _IOW(GPIO_IOCTL_MAGIC, 1, int)
+#define GPIO_IOCTL_GET_INPUT _IOW(GPIO_IOCTL_MAGIC, 2, int)
+#define GPIO_IOCTL_SET_PID_CMD _IOW(GPIO_IOCTL_MAGIC, 3, int)
+#define DFI_GPIO_DEV "/dev/GPIO_DEVICE"
 
-#define     GPO_0	                0
-#define     GPO_1	                1
-#define     SIG_TEST                44
+#define GPO_23 0
+#define GPO_26 1
+#define SIG_TEST 44
 
 
-void receiveData(int n , siginfo_t *info, void *unused){
-	
+void receiveData(int n, siginfo_t *info, void *unused) {
+
 	printf("receive GPE value: 0x%x\n", info->si_int);
-
-	return;
 }
 
-int main(int argc, char *argv[]){
+int main(int argc, char *argv[]) {
 
 	int fd;
 	int processPid;
-    unsigned char outByte , inByte;
+    unsigned char outbyte , inbyte;
 	struct sigaction sig;
 
 	sig.sa_sigaction = receiveData;
-	sig.sa_flags     = SA_SIGINFO;
-	
-	sigaction(SIG_TEST, &sig,NULL);
+	sig.sa_flags = SA_SIGINFO;
 
-	fd = open(DFI_GPIO_DEV , O_RDWR);
+	sigaction(SIG_TEST, &sig, NULL);
 
-	if(fd < 0){
+	fd = open(DFI_GPIO_DEV, O_RDWR);
+
+	if (fd < 0) {
 		printf("open dev fail: %d\n",fd);
 		return -1;
 	}
 
 	processPid = getpid();
-	printf("pid:%d\n",processPid);
+	printf("pid:%d\n", processPid);
 
-	if(ioctl(fd, GPIO_IOCTL_SET_PID_CMD , &processPid) < 0){
+	if (ioctl(fd, GPIO_IOCTL_SET_PID_CMD, &processPid) < 0) {
 		printf("ioctl set pid fail\n");
         goto ERROR_HANDLE;
-	}    
+	}
 
-    if(argc > 1){
+    if (argc > 1) {
+        outbyte = strtol(argv[1], NULL, 16);
 
-        outByte = strtol(argv[1] , NULL,16);
+		if (outbyte & (1 << GPO_23))
+			printf("ioctl set super io gpo_23 output\n");
 
-        if(ioctl(fd, GPIO_IOCTL_SET_OUTPUT , &outByte) < 0){
+		if (outbyte & (1 << GPO_26))
+			printf("ioctl set super io gpo_26 output\n");
+
+        if(ioctl(fd, GPIO_IOCTL_SET_OUTPUT , &outbyte) < 0){
 		    printf("ioctl set output fail\n");
             goto ERROR_HANDLE;
-	    }        
+	    }
     }
 
-    
-    if(ioctl(fd, GPIO_IOCTL_GET_INPUT , &inByte) < 0){
+    if (ioctl(fd, GPIO_IOCTL_GET_INPUT, &inbyte) < 0) {
         printf("ioctl get input fail\n");
         goto ERROR_HANDLE;
-    } 
-    
-    printf("input status: %02X\n\n",inByte);
+    }
+
+    printf("input status: %02X\n\n",inbyte);
 
     printf("wait...\n");
 
-    while(1){
-           usleep(1000000);
+    while (1) {
+		usleep(1000000);
     }
 
 
